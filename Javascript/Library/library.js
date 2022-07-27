f31b02ac5246aa97f1eba0bc93161cdc287d1fcf
Jordan <jordantannen@gmail.com>
2022-07-26 23:43:42 -0400

    Allow inserting of books into table

---
+++ Javascript/Library/library.js
@@ -0,0 +1,40 @@
+let myLibrary = []
+
+function Book(title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+}
+
+Book.prototype.getTitle = function(){
+    return this.title
+}
+
+Book.prototype.getAuthor = function(){
+    return this.author
+}
+
+Book.prototype.getPages = function(){
+    return this.pages
+}
+
+function addToLibrary(userBook) {
+    myLibrary.push(userBook)
+    myLibrary.sort()
+    
+    var libraryTable = document.getElementById("library")
+    
+    var newRow = libraryTable.insertRow(-1)
+    newRow.insertCell(0).innerHTML = userBook.getTitle()
+    newRow.insertCell(1).innerHTML = userBook.getAuthor()
+    newRow.insertCell(2).innerHTML = userBook.getPages()
+
+}
+
+let testBook = new Book("Gatsby", "Scott", 200)
+let testBook2 = new Book("Jame", "Scott", 200)
+let testBook3 = new Book("Ape", "Scott", 200)
+
+addToLibrary(testBook)
+addToLibrary(testBook2)
+addToLibrary(testBook3)
