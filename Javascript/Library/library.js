0b666adc6e45e13d0a96c07e4591167009e01bd5
Jordan <jordantannen@gmail.com>
2022-07-30 12:50:26 -0400

    Clean up JS

--- Javascript/Library/library.js
+++ Javascript/Library/library.js
@@ -1,5 +1,6 @@
 let myLibrary = []
 
+// Book Object and Methods
 function Book(title, author, pages, isRead) {
     this.title = title
     this.author = author
@@ -27,76 +28,87 @@ Book.prototype.changeReadStatus = function(status){
     this.isRead = status
 }
 
+// Add to library
 function addToLibrary(title, author, pages, isRead) {
-    console.log(isRead)
-    console.log(title)
 
+    // Creates new book, pushes to library
     let userBook = new Book(title, author, pages, isRead)
     myLibrary.push(userBook)
     myLibrary.sort()
 
+}
+
+function updateTable() {
+
+    // Pulls librarytable and creates a new row
     var libraryTable = document.getElementById("library")
-    
     var newRow = libraryTable.insertRow(-1)
     newRow.id = myLibrary.length - 1
-    newRow.insertCell(0).innerHTML = userBook.getTitle()
-    newRow.insertCell(1).innerHTML = userBook.getAuthor()
-    newRow.insertCell(2).innerHTML = userBook.getPages()
+    
+    // Inserts data into new rows
+    newRow.insertCell(0).innerHTML = myLibrary[newRow.id].getTitle()
+    newRow.insertCell(1).innerHTML = myLibrary[newRow.id].getAuthor()
+    newRow.insertCell(2).innerHTML = myLibrary[newRow.id].getPages()
 
+    // Creates the isRead checkbox
     let readBox = document.createElement("INPUT")
     readBox.setAttribute("type", "checkbox")
     newRow.append(readBox)
     
-    
-    if (userBook.getReadStatus()){
+    // Checks readbox if book has been read
+    if (myLibrary[newRow.id].getReadStatus()){
         readBox.checked = true
-        console.log(userBook.getReadStatus())
     }
 
+    // Updates readbox on user input
     readBox.onclick = () => {
-        if (readBox.checked == true)
-        {
+        if (readBox.checked)
             myLibrary[newRow.id].changeReadStatus(true)
-            console.log("test")
-        }
         else
-        {
             myLibrary[newRow.id].changeReadStatus(false)
-            console.log("test2")
-        }
-
     }
     
-    
+    // Creates delete button and ads to table
     let deleteButton = document.createElement("button")
     deleteButton.innerHTML = "Remove"
     newRow.append(deleteButton)
+
+    // Removes row based on user input
     deleteButton.onclick = () => {
         newRow.remove()
         myLibrary.splice(newRow.id, 1)
     }
-
 }
 
+
+// Get buttons and overlay
 let addBook = document.getElementById("add")
+let submitButton = document.getElementById("submit")
 let overlay = document.getElementById("overlay")
 
+// Makes form visible when addbook button is clicked
 addBook.addEventListener("click", () => {
     overlay.style.visibility = 'visible';
 });
 
-let submitButton = document.getElementById("submit")
-
+// Adds to library
 submitButton.addEventListener("click", () => {
+
+    // Determines if book has been read
     let isRead = false
-    console.log(isRead)
     if (document.getElementById("isRead").checked)
         isRead = true
-    console.log(isRead)
+
+    // Adds book to library array
     addToLibrary(document.getElementById("title").value, 
         document.getElementById("author").value, 
         document.getElementById("pages").value,
         isRead);
+
+    // Updates table
+    updateTable()
+
+    // Hides overlay
     overlay.style.visibility = 'hidden';
 });
 
