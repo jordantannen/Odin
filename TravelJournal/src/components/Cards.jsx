c4290e1abe0ecafc2c902946932520d67e85699b
Jordan <jordantannen@gmail.com>
2022-08-19 23:54:54 -0400

    Complete data importing

---
+++ TravelJournal/src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Card(props){
+    return (
+        <div className="card">
+            <img src={props.imageUrl} />
+            <div className="info">
+                <div className="location">
+                    <p>{props.location}</p>
+                    <a href={props.googleMapsUrl}></a>
+                </div>
+                <h3>{props.title}</h3>
+                <p className="dates">{props.startDate} - {props.endDate}</p>
+                <p className="desc">{props.description}</p>
+            </div>
+        </div>
+    )
+}
---
+++ TravelJournal/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(){
+    return (
+        <nav>
+            <div className="headerBox">
+                <p>my travel journal</p>
+            </div>
+        </nav>
+    )
+}
